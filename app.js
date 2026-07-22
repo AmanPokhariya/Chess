ac8ab666a1f07bce239fe7e3921ce97238f9e848
Aman Pokhariya <aman4191729@gmail.com>
2026-07-22 09:25:45 +0530

    Update title to Chess World and change port to 3005

--- app.js
+++ app.js
@@ -74,6 +74,6 @@ io.on("connection", function (uniquesocket) {
 
 });
 
-server.listen(3000, function () {
-    console.log("listening on port 3000")
+server.listen(3005, function () {
+    console.log("listening on port 3005")
 });
